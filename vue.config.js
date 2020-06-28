589eee99db41bd9318817847053d363b6829d2f7
Maksim996 <12usa34airbornE@gmail.com>
2020-06-28 14:56:23 +0300

    Create (form) Register.vue

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
 
 module.exports = {
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, 'src/')
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src/')
+      }
     }
   },
 
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: '(ru)',
+      fallbackLocale: 'en',
+      localeDir: '(lang1)',
+      enableInSFC: true
+    }
+  }
 };
