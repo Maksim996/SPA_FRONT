0b58db3de448392fd2d26bccf4ff116dacd7605c
Maksim996 <12usa34airbornE@gmail.com>
2020-07-02 16:50:29 +0300

    Create (i18n.js) add ua lang

---
+++ src/lang/ua.js
@@ -0,0 +1,10 @@
+export default {
+    t: {
+        loginIn: 'Авторизуватись',
+        register: 'Реєстрація',
+        password: 'Пароль',
+        login: 'Логін',
+        name: 'Ім\'я',
+
+    }
+}
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import VueI18n from 'vue-i18n';
 import ru from '@/lang/ru';
+import ua from '@/lang/ua';
 
 Vue.use(VueI18n);
 
 export default new VueI18n({
     locale: process.env.VUE_APP_I18N_LOCALE,
     messages: {
-        ru
+        ru, ua
     }
 })
