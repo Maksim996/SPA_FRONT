7aba246e9d0a4405f578d41caaa5da900867a037
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2020-07-02 23:06:06 +0300

    Renamed the file dashpoard to dashboard

---
+++ src/views/pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <TopBar></TopBar>
+        <Navigation></Navigation>
+        <v-main>
+            <v-container fluid>
+                <router-view></router-view>
+
+                <!--        <loader></loader>-->
+
+                <!--        <messages-info></messages-info>-->
+            </v-container>
+
+        </v-main>
+    </div>
+</template>
+
+<script>
+    import Navigation from "@/components/layout/Navigation.vue";
+    import TopBar from "@/components/layout/TopBar.vue";
+
+    export default {
+        name: "Dashpoard",
+        components: {
+            Navigation,
+            TopBar
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
