532f0a69b47e236233cf170c4b9cfdca76be4493
Maksim996 <12usa34airbornE@gmail.com>
2020-07-07 08:35:16 +0300

    Update (logo)

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -9,20 +9,11 @@
     <div class="d-flex align-center">
       <v-app-bar-nav-icon @click="() => drawer = !drawer"></v-app-bar-nav-icon>
       <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2 mx-4"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
+        alt="Medical center"
+        class="shrink hidden-sm-and-down"
         contain
         min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        src="images/logo/logo.svg"
         width="100"
       />
     </div>
