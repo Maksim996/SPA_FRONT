1b7f8961ecc73c07acc0915d63ae2508cf7e45d8
Maksim996 <12usa34airbornE@gmail.com>
2020-07-09 21:06:18 +0300

    Future (CreateDirector) Phones and email

---
+++ src/components/Phones.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      md="4"
+    >
+      <validation-provider v-slot="{ errors }" :name="$t('t.NumberMobPhone')" rules="required|phone">
+        <v-text-field
+          v-model="phone"
+          class="phone"
+          :label="$t('t.NumberMobPhone')"
+          name="Phone"
+          placeholder="+38(0**)-***-**-**"
+          prepend-icon="mdi-cellphone-android"
+          :error-messages="errors"
+          type="text"
+          v-mask="GlobalMaskPhone"
+        ></v-text-field>
+      </validation-provider>
+    </v-col>
+    <v-col
+      cols="12"
+      md="4"
+    >
+      <validation-provider
+        v-slot="{ errors }"
+        :name="$t('t.Additional') + ' ' +  $t('t.NumberMobPhone')"
+        rules="phone"
+      >
+        <v-text-field
+          v-model="additionalPhone"
+          class="phone"
+          :label="$t('t.Additional') + ' ' +  $t('t.NumberMobPhone')"
+          name="Phone"
+          placeholder="+38(0**)-***-**-**"
+          prepend-icon="mdi-cellphone-android"
+          :error-messages="errors"
+          type="text"
+          v-mask="GlobalMaskPhone"
+        ></v-text-field>
+      </validation-provider>
+    </v-col>
+  </v-row>
+
+</template>
+
+<script>
+  export default {
+    name: "Phones",
+    data() {
+      return {
+        phone: '',
+        additionalPhone: '',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/pages/Director/CreateDirector.vue
+++ src/views/pages/Director/CreateDirector.vue
@@ -3,7 +3,23 @@
     <validation-observer ref="formValidate" v-slot="{ validate, reset }">
       <form>
         <FirstSecondNames ref="FirstSecondNames"></FirstSecondNames>
+        <Phones ref="Phones"></Phones>
         <BirthdayDatePicker ref="BirthdayDatePicker"></BirthdayDatePicker>
+
+        <v-row>
+          <v-col cols="12" md="4">
+            <validation-provider v-slot="{ errors }" :name="$t('t.Email')" rules="required|email">
+              <v-text-field
+                v-model="email"
+                :label="$t('t.Email')"
+                name="Email"
+                prepend-icon="mdi-email"
+                type="text"
+                :error-messages="errors"
+              ></v-text-field>
+            </validation-provider>
+          </v-col>
+        </v-row>
 
 
         <!--      <v-text-field-->
@@ -30,35 +46,40 @@
     </validation-observer>
   </v-container>
 
-  <!--  <validation-provider></validation-provider>-->
 </template>
 
 <script>
   import FirstSecondNames from "@/components/FirstSecondNames";
   import BirthdayDatePicker from "@/components/BirthdayDatePicker";
+  import Phones from "@/components/Phones";
 
   export default {
     components:{
       FirstSecondNames,
       BirthdayDatePicker,
+      Phones,
     },
     name: "CreateDirector",
     data() {
       return {
         date: '',
         modal: false,
+        email: '',
       }
     },
     methods: {
       async submitForm() {
 
         const data = {
-          'first_name' : this.$refs.FirstSecondNames.firstName,
-          'second_name' : this.$refs.FirstSecondNames.secondName,
-          'patronimic' : this.$refs.FirstSecondNames.patronimic,
-          'birthday' : this.$refs.BirthdayDatePicker.date,
+          'first_name': this.$refs.FirstSecondNames.firstName,
+          'second_name': this.$refs.FirstSecondNames.secondName,
+          'patronimic': this.$refs.FirstSecondNames.patronimic,
+          'birthday': this.$refs.BirthdayDatePicker.date,
+          'email': this.email,
+          'phone': this.$refs.Phones.phone,
+          'additional_phone': this.$refs.Phones.additionalPhone,
         };
-
+        console.log(data);
         const valid = await this.$refs.formValidate.validate().then( (res) => res );
         console.log('valid',valid);
         valid ? console.log('data',data) : console.log('no data')
