0c865452cbb2dfb1abf298a7f2f99600a7c3c0ea
Maksim996 <12usa34airbornE@gmail.com>
2020-08-08 13:52:20 +0300

    Future (InnCode)

---
+++ src/components/InnCode.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="4">
+      <validation-provider v-slot="{ errors }" :name="$t('t.Inn_code')" rules="required|maskLength:10">
+        <v-text-field
+          v-model="InnCode"
+          :label="$t('t.Inn_code')"
+          name="Inn_code"
+          prepend-icon="mdi-email"
+          type="text"
+          :error-messages="errors"
+          v-mask="`###-###-####`"
+          placeholder="000-000-0000"
+        ></v-text-field>
+      </validation-provider>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: "InnCode",
+    data() {
+      return {
+        InnCode: '',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
