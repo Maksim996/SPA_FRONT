82e5db1306d2a13c2a3d3e23f6a5da336f6df059
Maksim996 <12usa34airbornE@gmail.com>
2020-08-08 23:29:33 +0300

    Future (TitlePage)

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-sheet
+    class="title_page
+      font-weight-medium
+      text-h5
+      text-lg-h4
+      mb-5
+      text-center
+      text-lg-start
+    "
+    color="headerCC--text"
+  >
+    {{title}}
+  </v-sheet>
+</template>
+
+<script>
+  export default {
+    name: "TitlePage",
+    props: [
+      'title'
+    ]
+  }
+</script>
+
+<style scoped>
+  .title_page {
+    background: transparent;
+  }
+</style>
