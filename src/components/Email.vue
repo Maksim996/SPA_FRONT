69b0247ac131183855863f2ec7b12aaefd570d30
Maksim996 <12usa34airbornE@gmail.com>
2020-10-12 11:53:09 +0300

    Feature (Email) create email component

---
+++ src/components/Email.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="4">
+      <validation-provider v-slot="{ errors }" :name="$t('t.Email')" rules="required|email">
+        <v-text-field
+          v-model="email"
+          :label="$t('t.Email')"
+          name="Email"
+          prepend-icon="mdi-email"
+          type="text"
+          :error-messages="errors"
+        ></v-text-field>
+      </validation-provider>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: "Email",
+    data() {
+      return {
+        email: this.emailProp,
+      }
+    },
+    props: {
+      emailProp: {
+        type: String,
+        default: ''
+      }
+    },
+  }
+</script>
