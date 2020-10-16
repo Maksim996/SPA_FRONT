7951ecefd4a1a5fae2499f77ec6e5971ef45cf91
Maksim996 <12usa34airbornE@gmail.com>
2020-10-16 21:16:17 +0300

    Feature (LoaderLinear)

---
+++ src/components/LoaderLinear.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loader-table">
+    <v-progress-linear
+      indeterminate
+      color="cyan"
+    ></v-progress-linear>
+    <span>{{$t('m.Loading')}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+name: "LoaderLinear"
+}
+</script>
+
+<style scoped>
+  .loader-table {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
