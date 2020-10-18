5f70998a0eddcb1591fd1d1291742f891cae4594
Maksim996 <12usa34airbornE@gmail.com>
2020-10-18 16:22:33 +0300

    Feature (Pagination) create component

---
+++ src/components/layout/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="max-width" v-if="data">
+    <v-pagination
+      v-model="page"
+      class="my-4"
+      color="btnCC"
+      :total-visible="PAGINATION.TotalVisible"
+      :length="this.data.total"
+    ></v-pagination>
+  </v-container>
+</template>
+
+<script>
+import { PAGINATION } from '@/utils/constants'
+
+export default {
+  name: "Pagination",
+  props: ['data'],
+  data(){
+    return {
+      page: this.$route.query.page ? Number(this.$route.query.page) : 1,
+      PAGINATION,
+    }
+  },
+  mounted() {
+  },
+  watch: {
+    page() {
+      const query = this.page ? `?page=${this.page}` : '';
+
+      this.$emit('update', query);
+
+      this.$router.replace({query: {
+          page: this.page
+        }});
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
